35cafed49d804b9d85c77ae20a85c1a90da8569f
whutao <whutao.dev@gmail.com>
2025-02-14 16:31:23 +0300

    Add isPressed environment key.

---
+++ Sources/SwiftUIExtras/Environment/Environment+isPressed.swift
@@ -0,0 +1,26 @@
+import SwiftUI
+
+extension EnvironmentValues {
+    
+    private enum IsPressedKey: EnvironmentKey {
+        static let defaultValue: Bool = false
+    }
+    
+    /// A Boolean value that indicates whether
+    /// the view associated with this environment is in the pressed state.
+    ///
+    /// The default value is false.
+    public var isPressed: Bool {
+        get { self[IsPressedKey.self] }
+        set { self[IsPressedKey.self] = newValue }
+    }
+}
+
+extension View {
+    
+    /// Sets the `isPressed` environment value.
+    @inlinable
+    public func isPressed(_ isPressed: Bool) -> some View {
+        environment(\.isPressed, isPressed)
+    }
+}
