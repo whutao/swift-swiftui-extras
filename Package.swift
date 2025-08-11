76f0a2fc03a4cd5e193490c46fbe8189dc7d4b48
whutao <whutao.dev@gmail.com>
2025-08-11 20:57:38 +0300

    Add CoreGraphicsExtras package dependency.

--- Package.swift
+++ Package.swift
@@ -10,7 +10,17 @@ let package = Package(
     products: [
         .library(name: "SwiftUIExtras", targets: ["SwiftUIExtras"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/whutao/swift-core-graphics-extras", from: Version(1, 0, 0)),
+        .package(url: "https://github.com/whutao/swift-extras", from: Version(2, 0, 0)),
+    ],
     targets: [
-        .target(name: "SwiftUIExtras")
+        .target(
+            name: "SwiftUIExtras",
+            dependencies: [
+                .product(name: "CoreGraphicsExtras", package: "swift-core-graphics-extras"),
+                .product(name: "SwiftExtras", package: "swift-extras")
+            ]
+        )
     ]
 )
